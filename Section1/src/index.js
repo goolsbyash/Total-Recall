a0689dc0067b6f06d43a014a466028b23fddd1b7
goolsbyash <goolsbyashley015@gmail.com>
2023-11-21 09:39:10 -0500

    Exercise B completed.

--- Section1/src/index.js
+++ Section1/src/index.js
@@ -6,10 +6,19 @@ let favoriteLanguage = 'Python';
 let newFavoriteLanguage = 'JavaScript';
 favoriteLanguage = newFavoriteLanguage;
 
-console.log(`Hi! My name is ${myName} and my favorite programming language is ${favoriteLanguage}`);
+console.log(`Hi! My name is ${myName} and my favorite programming language is ${favoriteLanguage}.`);
 console.log("=========================");
+
 // Exercise B: Strings
-let firstVariable = 'Hello World'
+let firstVariable = 'Hello World';
+firstVariable = 626;
+let secondVariable = firstVariable;
+secondVariable = 'Houston, do we have a problem?';
+
+console.log(firstVariable);
+
+let yourName = "Ashley Goolsby";
+console.log('Hello, my name is ' + yourName + '.');
 
 // Exercise C: Booleans
 
