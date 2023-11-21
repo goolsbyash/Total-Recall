925ec97d317fde5a7f5604da1ac24eb034f1f5ca
goolsbyash <goolsbyashley015@gmail.com>
2023-11-21 09:31:45 -0500

    Exercise A completed.

---
+++ Section1/src/index.js
@@ -0,0 +1,17 @@
+// Section 1: Variables & Data Types
+
+// Exercise A: Variables
+const myName = 'Ashley Goolsby';
+let favoriteLanguage = 'Python';
+let newFavoriteLanguage = 'JavaScript';
+favoriteLanguage = newFavoriteLanguage;
+
+console.log(`Hi! My name is ${myName} and my favorite programming language is ${favoriteLanguage}`);
+console.log("=========================");
+// Exercise B: Strings
+let firstVariable = 'Hello World'
+
+// Exercise C: Booleans
+
+
+// Exercise D: The Farm
