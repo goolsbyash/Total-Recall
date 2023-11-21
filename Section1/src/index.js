3b28be94db79b1918c74ae2405c65d64af49552f
goolsbyash <goolsbyashley015@gmail.com>
2023-11-21 10:09:21 -0500

    Section 2 Exercise A completed.

--- Section1/src/index.js
+++ Section1/src/index.js
@@ -1,69 +1,106 @@
-// Section 1: Variables & Data Types
+//* Section 1: Variables & Data Types
 
 // Exercise A: Variables
-const myName = 'Ashley Goolsby';
-let favoriteLanguage = 'Python';
-let newFavoriteLanguage = 'JavaScript';
+const myName = "Ashley Goolsby";
+let favoriteLanguage = "Python";
+let newFavoriteLanguage = "JavaScript";
 favoriteLanguage = newFavoriteLanguage;
 
-console.log(`Hi! My name is ${myName} and my favorite programming language is ${favoriteLanguage}.`);
+console.log(
+  `Hi! My name is ${myName} and my favorite programming language is ${favoriteLanguage}.`
+);
 console.log("=========================");
 
 // Exercise B: Strings
-let firstVariable = 'Hello World';
+let firstVariable = "Hello World";
 firstVariable = 626;
 let secondVariable = firstVariable;
-secondVariable = 'Houston, do we have a problem?';
+secondVariable = "Houston, do we have a problem?";
 
 console.log(firstVariable);
 
 let yourName = "Ashley Goolsby";
-console.log('Hello, my name is ' + yourName + '.');
+console.log("Hello, my name is " + yourName + ".");
+
+console.log("=========================");
 
 // Exercise C: Booleans
 const a = 4;
 const b = 53;
 const c = 57;
 const d = 16;
-const e = 'Kevin';
+const e = "Kevin";
 
 console.log(a < b);
 console.log(c > d);
-console.log('Name' === 'Name');
+console.log("Name" === "Name");
 // FOR THE NEXT TWO, USE ONLY && OR ||
 console.log(true || false);
-console.log(false && false && false && false && false || true);
+console.log((false && false && false && false && false) || true);
 console.log(false === false);
-console.log(e === 'Kevin');
+console.log(e === "Kevin");
 console.log(a < b + c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
 console.log(a * a === d); // note: the answer is a simple arithmetic equation, not something "weird"
-console.log(48 == '48');
+console.log(48 == "48");
 
+console.log("=========================");
 
 // Exercise D: The Farm
 
-let animal = 'cow';
+let animal = "cow";
 
 // Condition is false if the case is different
 // ex. animal = 'coW' != 'cow';
-// We may learn later how to best handle this 
+// We may learn later how to best handle this
 // scenario
 
 // adding 'COW' to condition too
-if ((animal === 'cow') || (animal === 'COW')) {
-    console.log('mooooo');
+if (animal === "cow" || animal === "COW") {
+  console.log("mooooo");
 } else {
-    console.log('Hey! You\'re not a cow.');
+  console.log("Hey! You're not a cow.");
 }
+
+console.log("=========================");
 
 // Exercise E: Driver's Ed
 
 let driverAge = 15;
 
 if (driverAge >= 16) {
-    console.log('Here are the keys!');
-} else if (driverAge >=0) {
-    console.log('Sorry, you\'re too young.');
+  console.log("Here are the keys!");
+} else if (driverAge >= 0) {
+  console.log("Sorry, you're too young.");
 } else {
-    console.log('Enter age as a positive number.');
+  console.log("Enter age as a positive number.");
 }
+
+console.log("=========================");
+
+
+//* Section 2: Loops
+
+// Exercise A: The Basics
+
+// print all nums 0- 10 inclusive
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+// print all nums 10 - 400 inclusive
+for (let i = 10; i <= 400; i++) {
+    console.log(i);
+}
+
+// print every 3rd num starting at 12 up to 4000
+for (let i = 12; i < 4000; i+=3) {
+    console.log(i);
+}
+
+console.log("=========================");
+
+// Exericse B: Get Even
+
+// Exercise C: Give Me Five
+
+// Exercise D: Savings Account
